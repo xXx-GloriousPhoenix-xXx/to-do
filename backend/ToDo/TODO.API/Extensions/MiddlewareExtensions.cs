5a1d54d4eb00b6a11c93b16460ddaaa8a6626948
RomanPR <prikhodjko.roman@gmail.com>
2026-06-30 15:17:59 +0300

    Middleware added

---
+++ backend/ToDo/TODO.API/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,11 @@
+﻿using TODO.API.Middleware;
+
+namespace TODO.API.Extensions;
+
+public static class MiddlewareExtensions
+{
+    public static IApplicationBuilder UseExceptionHandling(this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<ExceptionMiddleware>();
+    }
+}
--- backend/ToDo/TODO.API/Program.cs
+++ backend/ToDo/TODO.API/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddSwaggerDocs();
 
 var app = builder.Build();
 
+app.UseExceptionHandling();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
